f3c77cf00df949aa9e7b753c0352baf713cb5693
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2023-10-10 11:41:57 -0300

    Implementando Novo Usuario e Score do mesmo

--- novo_usuario.js
+++ novo_usuario.js
@@ -1,34 +1,43 @@
 function configuracao(){
+	var inicia_jogo = document.getElementById("inicia_jogo");
+	var novo_usuario = document.getElementById("novo_usuario");
+	if(localStorage.getItem("maiorScore") === null || localStorage.getItem("maiorScore") == 0 ){
 	//Esconde o Elemento do Canvas
-	inicia_jogo = document.getElementById("inicia_jogo");
-	inicia_jogo.Style.Display = "none";
+		inicia_jogo.style.display = "none";
 	//Mostra o Form para registrar o usuário;
-	novo_usuario = document.getElementById("novo_usuario");
-	novo_usuario.Style.Display = "none";
+		novo_usuario.style.display = "block";
+	}else{
+		console.log(localStorage.getItem("maiorScore"))
+		//Esconde o Form para registrar o usuário;
+		novo_usuario.style.display = "none";
+		//Mostra o Elemento do Canvas
+		inicia_jogo.style.display = "block";
+	}
 }
 function novoUsuario(){
-	inicia_jogo = document.getElementById("inicia_jogo");
-	novo_usuario = document.getElementById("novo_usuario");
+	var inicia_jogo = document.getElementById("inicia_jogo");
+	var novo_usuario = document.getElementById("novo_usuario");
 	// Obtém o valor do campo de entrada
 	var nomeUsuario = document.getElementById("nome").value;
 	// Armazena o nome do usuário na variável de sessão (localStorage)
 	localStorage.setItem("nomeUsuario", nomeUsuario);
-	alert(localStorage.getItem("maiorScore"));
+	//alert(localStorage.getItem("maiorScore"));
 	if(localStorage.getItem("maiorScore") !== null || localStorage.getItem("maiorScore") != 0 )
 		localStorage.setItem("maiorScore",localStorage.getItem("maiorScore"));
 	else{
 		localStorage.setItem("maiorScore",0);
 		alert(localStorage.getItem("maiorScore"));
 	}
-	
+	// Start the game loop
+	update();
 	// Redireciona para outra página ou realiza qualquer outra ação necessária
 	//alert("Nome do usuário salvo com sucesso!");
 
 	// Você pode redirecionar para outra página usando 
 	//window.location.href, por exemplo:
 	 //window.location.href = "index.html";
-	novo_usuario.Style.Display = "none";
-	inicia_jogo.Style.Display = "block";
+	/* novo_usuario.style.display = "none";
+	inicia_jogo.style.display = "block"; */
 			
 			 
         
--- script.js
+++ script.js
@@ -1,21 +1,68 @@
-const canvas = document.getElementById("game-canvas");
-const ctx = canvas.getContext("2d");
+function configuracao(){
+	var inicia_jogo = document.getElementById("inicia_jogo");
+	var novo_usuario = document.getElementById("novo_usuario");
+	console.log("teste");	
+	console.log(localStorage.getItem("maiorScore"));
+	if(localStorage.getItem("maiorScore") !== null){
+	console.log("é nulo");
+	console.log(localStorage.getItem("maiorScore"));
+	//Esconde o Elemento do Canvas
+		inicia_jogo.style.display = "none";
+	//Mostra o Form para registrar o usuário;
+		novo_usuario.style.display = "block";
+	}else{
+			console.log("não é nulo");
+			console.log(localStorage.getItem("maiorScore"));
+		//Esconde o Form para registrar o usuário;
+		novo_usuario.style.display = "none";
+		//Mostra o Elemento do Canvas
+		inicia_jogo.style.display = "block";
+	}
+}
+function novoUsuario(){
+	var inicia_jogo = document.getElementById("inicia_jogo");
+	var novo_usuario = document.getElementById("novo_usuario");
+	// Obtém o valor do campo de entrada
+	var nomeUsuario = document.getElementById("nome").value;
+	// Armazena o nome do usuário na variável de sessão (localStorage)
+	localStorage.setItem("nomeUsuario", nomeUsuario);
+	//alert(localStorage.getItem("maiorScore"));
+	if(localStorage.getItem("maiorScore") !== null || localStorage.getItem("maiorScore") != 0 )
+		localStorage.setItem("maiorScore",localStorage.getItem("maiorScore"));
+	else{
+		localStorage.setItem("maiorScore",0);
+		alert(localStorage.getItem("maiorScore"));
+	}
+	const canvas = document.getElementById("game-canvas");
+	const ctx = canvas.getContext("2d");
 
-const tRex = {
-    x: 50,
-    y: canvas.height - 40,
-    width: 40,
-    height: 40,
-    jumping: false,
-    jumpHeight: 150,
-    jumpSpeed: 5
-	
-};
+	const tRex = {
+		x: 50,
+		y: canvas.height - 40,
+		width: 40,
+		height: 40,
+		jumping: false,
+		jumpHeight: 150,
+		jumpSpeed: 5
+		
+	};
 
-const obstacles = [];
+	const obstacles = [];
 
-let score = 0;
-let isGameOver = false;
+	let score = 0;
+	let isGameOver = false;
+	// Start the game loop
+	update();
+	// Redireciona para outra página ou realiza qualquer outra ação necessária
+	//alert("Nome do usuário salvo com sucesso!");
+
+	// Você pode redirecionar para outra página usando 
+	//window.location.href, por exemplo:
+	 //window.location.href = "index.html";
+	/* novo_usuario.style.display = "none";
+	inicia_jogo.style.display = "block"; */
+			
+}
 
 // Function to draw the T-Rex
 function drawTrex() {
@@ -45,7 +92,7 @@ function update() {
 
         // Draw and update obstacles
         drawObstacles();
-        updateObstacles();
+		updateObstacles();
 
         // Update the score
         ctx.fillStyle = "black";
@@ -140,5 +187,4 @@ document.addEventListener("keydown", function (event) {
     }
 });
 
-// Start the game loop
-update();
+
